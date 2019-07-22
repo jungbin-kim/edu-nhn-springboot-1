4c35da466433ff317eb66072ad8f5a881683fd8c
Jungbin Kim <mail@jungbin.kim>
2019-07-22 14:43:07 +0900

    Init Spring boot base on maven
    - port 9000
    - GET /hello

---
+++ src/main/java/com/nhn/edu/springboot/ExampleApplication.java
@@ -0,0 +1,21 @@
+package com.nhn.edu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // RestController의 역할을 하게 해준다.
+@SpringBootApplication
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
