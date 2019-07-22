f8507f684ff034feb459308ebb134f9217b3f009
Jungbin Kim <mail@jungbin.kim>
2019-07-22 15:49:03 +0900

    Custom banner

--- src/main/java/com/nhn/edu/springboot/ExampleApplication.java
+++ src/main/java/com/nhn/edu/springboot/ExampleApplication.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // RestController 역할을 하게 해준다.
 @SpringBootApplication
 public class ExampleApplication {
+    // `src/main/resource/banner.txt` 파일을 생성하여 기본 banner 대체
+    // http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type Something
 
     public static void main(String[] args) {
+        // args가 없어도 컴파일은 되지만, spring boot 옵션들이 적용이 안된다.
         SpringApplication.run(ExampleApplication.class, args);
     }
 
